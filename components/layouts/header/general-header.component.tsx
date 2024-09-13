a3537e0b46b1b4f440a90cd02d4bb7de70b50cf1
Paulo Pintos <106354483+GodSavitar19@users.noreply.github.com>
2024-09-13 15:03:51 -0300

    styles Header

--- components/layouts/header/general-header.component.tsx
+++ components/layouts/header/general-header.component.tsx
@@ -5,73 +5,61 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Container from "@mui/material/Container";
 import NextLink from "next/link";
-import { Link as MUILink, styled } from "@mui/material";
+import { Link as MUILink } from "@mui/material";
 
 type Props = {
   variant?: "simple" | "general";
 };
 
-const HeaderLink = styled(MUILink)(({ theme }) => ({
-  color: theme.palette.common.white,
-  textDecoration: "none",
-  "&:hover": {
-    color: theme.palette.secondary.light,
-  },
-}));
-
 const Header: FC<Props> = ({ variant }: Props) => {
   return (
-    <Toolbar disableGutters>
-      <NextLink href="/" passHref>
-        <HeaderLink
-          variant="h6"
-          sx={{
-            mr: 2,
-            flexGrow: 1,
-            fontWeight: 700,
-            fontSize: 24,
-          }}
-        >
-          DH-Marvel
-        </HeaderLink>
-      </NextLink>
-      {variant === "general" && (
-        <Box>
-          <NextLink href="/faqs" passHref>
-            <HeaderLink
-              variant="body2"
-              sx={{
-                mr: 2,
-                fontSize: 18,
-                fontWeight: 600,
-              }}
-            >
-              FAQ
-            </HeaderLink>
-          </NextLink>
-        </Box>
-      )}
-    </Toolbar>
+    <Container maxWidth="xl">
+      <Toolbar disableGutters>
+        <NextLink href="/" passHref>
+          <MUILink
+            variant="h6"
+            sx={{
+              mr: 2,
+              flexGrow: 1,
+              fontWeight: 700,
+              color: "inherit",
+              textDecoration: "none",
+            }}
+          >
+            DH-Marvel
+          </MUILink>
+        </NextLink>
+        {variant === "general" && (
+          <Box>
+            <NextLink href="/faqs" passHref>
+              <MUILink
+                variant="body2"
+                sx={{ color: "white", fontSize: 18, fontWeight: 600 }}
+              >
+                FAQ
+              </MUILink>
+            </NextLink>
+          </Box>
+        )}
+      </Toolbar>
+    </Container>
   );
 };
 
 const GeneralHeader: FC<Props> = ({ variant }: Props) => {
-  return (
+  return variant === "general" ? (
     <AppBar
       position="static"
       sx={{
-        backgroundColor: "#1976d2", // Azul principal del tema
-        color: "#fff",
-        boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)", // Sombra para profundidad
+        backgroundColor: "#1976d2", 
       }}
     >
-      <Container maxWidth="xl">
-        <Header variant={variant} />
-      </Container>
+      <Header variant={variant} />
     </AppBar>
+  ) : (
+    <Header variant={variant} />
   );
 };
-
 GeneralHeader.defaultProps = {
   variant: "general",
 };
